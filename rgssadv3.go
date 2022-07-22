4d8f73f34600229b088163802aa9a44b736ceef2
xubiod <xubiod.mail@gmail.com>
2022-07-21 21:13:34 -0400

    still untested as of now, but all files here just need to be tested! oh lord

--- rgssadv3.go
+++ rgssadv3.go
@@ -1,16 +1,17 @@
 package rpgo
 
 import (
+	"encoding/binary"
 	"fmt"
 	"io"
 )
 
-//RPGMakerDecrypter.Decrypter/RGSSADv1.cs
+//RPGMakerDecrypter.Decrypter/RGSSADv3.cs
 
-type RGSSADv1 RGSSAD
+type RGSSADv3 RGSSAD
 
-func (RGSSADv1) Make(filepath string) (*RGSSADv1, error) {
-	created := new(RGSSADv1)
+func (RGSSADv3) Make(filepath string) (*RGSSADv3, error) {
+	created := new(RGSSADv3)
 	created.Filepath = filepath
 
 	version, err := ((*RGSSAD)(created)).GetVersion()
@@ -24,15 +25,28 @@ func (RGSSADv1) Make(filepath string) (*RGSSADv1, error) {
 	return created, nil
 }
 
-func (rpg *RGSSADv1) readRGSSAD() {
-	key := uint(RGASSADv1Key)
-
+func (rpg *RGSSADv3) readRGSSAD() {
 	rpg.ByteReader.Seek(8, io.SeekStart)
+
+	t := make([]byte, 4)
+	rpg.ByteReader.Read(t)
+	var num int
+	for _, e := range t {
+		num = num << 8
+		num |= int(e)
+	}
+
+	key := uint(num)
+
+	key *= 9
+	key += 3
+
+	// key := uint(RGASSADv1Key)
 
 	for {
 		newArchivedFile := new(RPGMakerArchivedFile)
 
-		t := make([]byte, 4)
+		t = make([]byte, 4)
 		rpg.ByteReader.Read(t)
 		var num int
 		for _, e := range t {
@@ -74,27 +88,32 @@ func (rpg *RGSSADv1) readRGSSAD() {
 	}
 }
 
-func (*RGSSADv1) decryptInteger(value int, key *uint) int {
-	result := value ^ int(*key)
-	*key *= 7
-	*key += 3
+func (*RGSSADv3) decryptInteger(value int, key *uint) int {
+	result := uint(value) ^ *key
+	// *key *= 7
+	// *key += 3
 
-	return result
+	return int(result)
 }
 
-func (*RGSSADv1) decryptFilename(encryptedName []byte, key *uint) string {
-	//decryptedName := make([]byte, len(encryptedName))
+func (*RGSSADv3) decryptFilename(encryptedName []byte, key *uint) string {
 	var decryptedName string
 
+	keyBytes := make([]byte, 4)
+	binary.LittleEndian.PutUint32(keyBytes, uint32(*key))
 	i := 0
+	j := 0
 
 	for i < len(encryptedName) {
-		decryptedName += string(rune(encryptedName[i] ^ byte(*key&0xFF)))
+		// if j == 4 {
+		// 	j = 0
+		// }
+		j %= 4
 
-		*key *= 7
-		*key += 3
+		decryptedName += string(rune(encryptedName[i] ^ keyBytes[j]))
 
 		i++
+		j++
 	}
 
 	return decryptedName
