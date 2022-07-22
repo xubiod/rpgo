52c0a6de988a88b9ce6314a547992c7879e9ae5c
xubiod <xubiod.mail@gmail.com>
2022-07-22 16:46:34 -0400

    thanks seemingly random errors + redundant tests

--- README.md
+++ README.md
@@ -4,9 +4,19 @@ This is currently a direct translation of uuksuu's RPGMakerDecrypter project,
 specifically the Decrypter namespace and the Tests namespace. You can go to that
 version [here](https://github.com/uuksu/RPGMakerDecrypter/).
 
-This was mainly an exercise with Go and the testing capabilities. All current
-tests pass. As of writing I want to implement more tests, as current coverage is
-at 58.9%.
+This was mainly an exercise with Go and the testing capabilities.
+
+## Tests
+
+**Current coverage as reported by `go test -cover`: 75.1%** 
+
+Currently all tests that were in the C# original have been implemented and all
+pass.
+
+Tests were added for encrypted file extracting for all supported versions. This
+is redundant but all tests pass.
+
+## Disclaimer
 
 **The untested functions are not guaranteed to work, period. Do not use this as**
 **a basis for any production-level project yet.**
--- rgssad_test.go
+++ rgssad_test.go
@@ -27,7 +27,7 @@ func TestExtractAllFiles_XP(t *testing.T) {
 		t.FailNow()
 	}
 
-	extractTo, err := os.MkdirTemp(os.TempDir(), "rpgo-*")
+	extractTo, err := os.MkdirTemp(os.TempDir(), "rpgo-testsuite-*")
 
 	if err != nil {
 		t.Errorf("test/ExtractAllFiles_XP: mkdir temp err not nil:\n%s", err.Error())
@@ -79,6 +79,156 @@ func TestExtractAllFiles_XP(t *testing.T) {
 
 		if !bytes.Equal(question_data, answer_data) {
 			t.Errorf("test/ExtractAllFiles_XP: file data mismath at index %d (sorted alphabetically)", idx)
+			t.FailNow()
+		}
+	}
+}
+
+func TestExtractAllFiles_VX(t *testing.T) {
+	extracted_answers := "./test-rsrc/extracted/vx/"
+
+	var goat *RGSSADv1
+	testArchive := "./test-rsrc/Game.rgss2a"
+
+	if _, err := os.Stat(testArchive); errors.Is(err, os.ErrNotExist) {
+		t.Error("test/ExtractAllFiles_VX: test config fail, archive doesn't exist")
+		t.FailNow()
+	}
+
+	goat, err := MakeRGSSADv1(testArchive)
+
+	if err != nil {
+		t.Errorf("test/ExtractAllFiles_VX: make rgssad err not nil:\n%s", err.Error())
+		t.FailNow()
+	}
+
+	extractTo, err := os.MkdirTemp(os.TempDir(), "rpgo-testsuite-*")
+
+	if err != nil {
+		t.Errorf("test/ExtractAllFiles_VX: mkdir temp err not nil:\n%s", err.Error())
+		t.FailNow()
+	}
+
+	err = (*RGSSAD)(goat).ExtractAllFiles(extractTo, true)
+
+	if err != nil {
+		t.Errorf("test/ExtractAllFiles_VX: extract err not nil:\n%s", err.Error())
+		t.FailNow()
+	}
+
+	// use os.SameFile(os.Stat(question), os.Stat(answer))
+
+	all_questions, err := os.ReadDir(filepath.Join(extractTo, "Data"))
+
+	if err != nil {
+		t.Errorf("test/ExtractAllFiles_VX: readdir for questions err not nil:\n%s", err.Error())
+		t.FailNow()
+	}
+
+	all_answers, err := os.ReadDir(filepath.Join(extracted_answers, "Data"))
+
+	if err != nil {
+		t.Errorf("test/ExtractAllFiles_VX: readdir for answers err not nil:\n%s", err.Error())
+		t.FailNow()
+	}
+
+	if len(all_questions) != len(all_answers) {
+		t.Errorf("test/ExtractAllFiles_VX: file count mismatch; expected %d, got %d", len(all_answers), len(all_questions))
+		t.FailNow()
+	}
+
+	for idx := range all_questions {
+		question_data, err := os.ReadFile(filepath.Join(extractTo, "Data", all_questions[idx].Name()))
+
+		if err != nil {
+			t.Errorf("test/ExtractAllFiles_VX: err getting question data at index %d (sorted alphabetically):\n%s", idx, err.Error())
+			t.FailNow()
+		}
+
+		answer_data, err := os.ReadFile(filepath.Join(extracted_answers, "Data", all_answers[idx].Name()))
+
+		if err != nil {
+			t.Errorf("test/ExtractAllFiles_VX: err getting answer data at index %d (sorted alphabetically):\n%s", idx, err.Error())
+			t.FailNow()
+		}
+
+		if !bytes.Equal(question_data, answer_data) {
+			t.Errorf("test/ExtractAllFiles_VX: file data mismath at index %d (sorted alphabetically)", idx)
+			t.FailNow()
+		}
+	}
+}
+
+func TestExtractAllFiles_VXAce(t *testing.T) {
+	extracted_answers := "./test-rsrc/extracted/vxa/"
+
+	var goat *RGSSADv3
+	testArchive := "./test-rsrc/Game.rgss3a"
+
+	if _, err := os.Stat(testArchive); errors.Is(err, os.ErrNotExist) {
+		t.Error("test/ExtractAllFiles_VXAce: test config fail, archive doesn't exist")
+		t.FailNow()
+	}
+
+	goat, err := MakeRGSSADv3(testArchive)
+
+	if err != nil {
+		t.Errorf("test/ExtractAllFiles_VXAce: make rgssad err not nil:\n%s", err.Error())
+		t.FailNow()
+	}
+
+	extractTo, err := os.MkdirTemp(os.TempDir(), "rpgo-testsuite-*")
+
+	if err != nil {
+		t.Errorf("test/ExtractAllFiles_VXAce: mkdir temp err not nil:\n%s", err.Error())
+		t.FailNow()
+	}
+
+	err = (*RGSSAD)(goat).ExtractAllFiles(extractTo, true)
+
+	if err != nil {
+		t.Errorf("test/ExtractAllFiles_VXAce: extract err not nil:\n%s", err.Error())
+		t.FailNow()
+	}
+
+	// use os.SameFile(os.Stat(question), os.Stat(answer))
+
+	all_questions, err := os.ReadDir(filepath.Join(extractTo, "Data"))
+
+	if err != nil {
+		t.Errorf("test/ExtractAllFiles_VXAce: readdir for questions err not nil:\n%s", err.Error())
+		t.FailNow()
+	}
+
+	all_answers, err := os.ReadDir(filepath.Join(extracted_answers, "Data"))
+
+	if err != nil {
+		t.Errorf("test/ExtractAllFiles_VXAce: readdir for answers err not nil:\n%s", err.Error())
+		t.FailNow()
+	}
+
+	if len(all_questions) != len(all_answers) {
+		t.Errorf("test/ExtractAllFiles_VXAce: file count mismatch; expected %d, got %d", len(all_answers), len(all_questions))
+		t.FailNow()
+	}
+
+	for idx := range all_questions {
+		question_data, err := os.ReadFile(filepath.Join(extractTo, "Data", all_questions[idx].Name()))
+
+		if err != nil {
+			t.Errorf("test/ExtractAllFiles_VXAce: err getting question data at index %d (sorted alphabetically):\n%s", idx, err.Error())
+			t.FailNow()
+		}
+
+		answer_data, err := os.ReadFile(filepath.Join(extracted_answers, "Data", all_answers[idx].Name()))
+
+		if err != nil {
+			t.Errorf("test/ExtractAllFiles_VXAce: err getting answer data at index %d (sorted alphabetically):\n%s", idx, err.Error())
+			t.FailNow()
+		}
+
+		if !bytes.Equal(question_data, answer_data) {
+			t.Errorf("test/ExtractAllFiles_VXAce: file data mismath at index %d (sorted alphabetically)", idx)
 			t.FailNow()
 		}
 	}
